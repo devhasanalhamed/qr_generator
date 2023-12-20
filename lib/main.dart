ecc355cb9889766807bcc55ea1e02f3892b8da63
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-20 09:05:05 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
